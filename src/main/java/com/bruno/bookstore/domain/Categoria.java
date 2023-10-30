1960516de1452ef0e509d276706f5a7c4d054260
Brunofell <brunofelimarti@gmail.com>
2023-10-30 14:00:21 -0300

    classes de dominio

---
+++ src/main/java/com/bruno/bookstore/domain/Categoria.java
@@ -0,0 +1,78 @@
+package com.bruno.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Categoria {
+    private Integer id;
+    private String nome;
+    private String descricao;
+
+    private List<Livro> livros = new ArrayList<>();
+
+    public Categoria() {
+        super();
+    }
+
+    public Categoria(Integer id, String nome, String descricao) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        if(id == null){
+            if(categoria.id != null){
+                return false;
+            }
+        }else if(!id.equals(categoria.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+}
